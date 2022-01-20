5a44dc9e94089ac6a489f825398c7081b52ac82d
Marcello Bozzi <bozzimarcello@gmail.com>
2022-01-20 12:23:43 +0100

    Modifica della classe Note
    Aggiunta dei campi per gestire la password

--- DecisiNotesFinale/Notes/Notes/Notes/Models/Note.cs
+++ DecisiNotesFinale/Notes/Notes/Notes/Models/Note.cs
@@ -7,7 +7,10 @@ namespace Notes.Models
     {
         [PrimaryKey, AutoIncrement]
         public int ID { get; set; }
-        public string Text { get; set; }
+        public string ServiceName { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string URL { get; set; }
         public DateTime Date { get; set; }
     }
 }
